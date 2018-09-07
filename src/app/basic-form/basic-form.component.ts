e51cb4425ed910b9d9562b74918c6ad08680672a
ducnt <dtno1.study@gmail.com>
2018-09-08 00:02:24 +0700

    het formbasic

--- src/app/basic-form/basic-form.component.ts
+++ src/app/basic-form/basic-form.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as o from 'rxjs';
 @Component({
   selector: 'app-basic-form',
   templateUrl: './basic-form.component.html',
   styleUrls: ['./basic-form.component.css']
 })
 export class BasicFormComponent implements OnInit {
-
+  @ViewChild('formRef') form;
 
   username: 'Simple1';
   onSubmit(formValue) {
-    alert(formValue);
+
     console.log(formValue);
   }
-  constructor() { }
   ngOnInit() {
+
+  }
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngAfterViewInit() {
+    this.form.valueChanges
+      .subscribe(v => console.table(v));
+
   }
 
 }
